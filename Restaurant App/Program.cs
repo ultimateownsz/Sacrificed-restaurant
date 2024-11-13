bd7f19d34c7262133c9718f89c307e04b132f8d2
ultimateownsz <saiutechnologies@outlook.com>
2024-11-13 01:25:26 +0100

    forgot to add the adminmenu case for theme functionality

--- Restaurant App/Program.cs
+++ Restaurant App/Program.cs
@@ -9,7 +9,8 @@ class Program
         Console.WriteLine("Starting Calendar Navigation...");
         
         // Start the calendar navigation to test the calendar and reservation functionality
-        MakingReservations.CalendarNavigation();
+        // MakingReservations.CalendarNavigation();
+        AdminMenu.AdminStart();
         
         Console.WriteLine("Thank you for using the Reservation System. Goodbye!");
     }
