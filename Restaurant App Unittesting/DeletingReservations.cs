e9044a670ffb06f622c6a517a84f07ce8b22a017
Hani <hanimohamedhm911@gmail.com>
2024-11-25 12:22:21 +0100

    unfinished unitest for deleting reservations

---
+++ Restaurant App Unittesting/DeletingReservations.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Restaurant_App_Unittesting
+{
+    // Unit Tests for Reservation Logic(deleting reservations)
+    [TestClass]
+    public class DeletingReservations
+    {
+
+    }
+}
