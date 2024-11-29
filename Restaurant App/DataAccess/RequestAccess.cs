16de882dc141b7444c4b022e55a7111887c0a38f
damariion <da.abendanon@gmail.com>
2024-11-29 19:06:53 +0100

    replaced the old data-access class with a new dynamic-class that inherits functionality from the base data-access class applying generics in the structure. This class in a separate file will allow the flexibility of adding specialized methods for this table. Additionally I've moved a specialized method that was initially found in the older data-access system

--- Restaurant App/DataAccess/RequestAccess.cs
+++ Restaurant App/DataAccess/RequestAccess.cs
@@ -5,7 +5,7 @@ public class RequestAccess : DataAccess<RequestModel>
 {
     public RequestAccess() : base(typeof(RequestModel).GetProperties().Select(p => p.Name).ToArray()) { }
 
-    // compatibility-integration
+    // compatibility/integration
     public Int64 GetLatestOrderID()
     {
         string sql = $"SELECT MAX(orderID) FROM {_table}";
