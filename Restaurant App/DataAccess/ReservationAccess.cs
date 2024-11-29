036ef5b46682fdc4c1c07261985ca312f549001c
damariion <da.abendanon@gmail.com>
2024-11-29 19:07:19 +0100

    replaced the old data-access class with a new dynamic-class that inherits functionality from the base data-access class applying generics in the structure. This class in a separate file will allow the flexibility of adding specialized methods for this table

--- Restaurant App/DataAccess/ReservationAccess.cs
+++ Restaurant App/DataAccess/ReservationAccess.cs
@@ -2,4 +2,5 @@
 public class ReservationAccess : DataAccess<ReservationModel>
 {
     public ReservationAccess() : base(typeof(ReservationModel).GetProperties().Select(p => p.Name).ToArray()) { }
+
 }
