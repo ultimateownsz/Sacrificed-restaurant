c2d41594c8427fd2f41a57e7ab39d52580ec95ff
Davidek523 <davidov520@wp.pl>
2024-12-06 18:24:41 +0100

    Adding selectionPresent Show
    Changed:
    - Revised the code of hani where he uses navigation with arrow keys the old way and added the SelectionPresent Show method.

--- Restaurant App/Presentation/FuturePastReservations.cs
+++ Restaurant App/Presentation/FuturePastReservations.cs
@@ -1,3 +1,4 @@
+using System.Formats.Asn1;
 using Project;
 using Project.Presentation;
 
@@ -121,8 +122,6 @@ namespace Presentation
                 switch (SelectionPresent.Show(["Past reservations", "Future reservations", "Cancel"], banner).text)
                 {
                     case "Past reservations":
-                        int reservationIndex = 0;
-                        bool inResMenu = true;
 
                         var userReservations = Access.Reservations.GetAllBy<int?>("UserID", acc.ID)
                                                                 .Where(r => r != null)
@@ -141,62 +140,21 @@ namespace Presentation
                             return;
                         }
 
-                        while (inResMenu)
+                        System.Console.Clear();
+                        System.Console.WriteLine($"Here are your past reservations, {acc.FirstName}");
+
+                        var pastOptions = userReservations.Select(r => $"Reservation: {r.Date} (ID: {r.ID})").ToList();
+                        var selectedPast = SelectionPresent.Show(pastOptions, "PAST RESERVATIONS\n\n").text;
+
+                        if (pastOptions.Contains(selectedPast))
                         {
-                            System.Console.Clear();
-                            System.Console.WriteLine($"Here are your past reservations, {acc.FirstName}");
-
-                            for (int j = 0; j < userReservations.Count; j++)
-                            {
-                                if (j == reservationIndex)
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Yellow;
-                                    Console.WriteLine($"> Reservation: {userReservations[j].Date}"); // Highlight selected item
-                                    Console.ResetColor();
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"  Reservation: {userReservations[j].Date}");
-                                }
-                            }
-
-                            var key = Console.ReadKey(intercept: true);
-                            switch (key.Key)
-                            {
-                                case ConsoleKey.UpArrow:
-                                    if (reservationIndex > 0) reservationIndex--; // Move up
-                                    break;
-
-                                case ConsoleKey.DownArrow:
-                                    if (reservationIndex < userReservations.Count - 1) reservationIndex++; // Move down
-                                    break;
-
-                                case ConsoleKey.Enter:
-                                    // Process the selected reservation
-                                    Console.Clear();
-                                    Console.WriteLine($"You selected Reservation on: {userReservations[reservationIndex].Date}");
-                                    Console.WriteLine("Press any key to return to the reservation overview menu or press Escape to return to the main menu...");
-                                    var key2 = Console.ReadKey();
-
-                                    if (key2.Key == ConsoleKey.Escape)
-                                    {
-                                        inResMenu = false;
-                                        break; // Exit loop
-                                    }
-                                    else
-                                    {
-                                        break;
-                                    }
-
-                                case ConsoleKey.Escape: // Exit without selection
-                                    inResMenu = false;
-                                    break;
-                            }
+                            Console.Clear();
+                            System.Console.WriteLine($"You selected Reservation on: {pastOptions})");
+                            Console.WriteLine("Press any key to return to the reservation overview menu or press Escape to return to the main menu...");
+                            Console.ReadKey();
                         }
                         break;
                     case "Future reservations":
-                        bool isValid = true;
-                        int futureIndex = 0;
 
                         var futureReservations = Access.Reservations.GetAllBy<int?>("UserID", acc.ID)
                                                                 .Where(r => r != null)
@@ -215,57 +173,18 @@ namespace Presentation
                             return;
                         }
 
-                        while (isValid)
+                        Console.Clear();
+                        System.Console.WriteLine($"Here are your current and future reservations, {acc.FirstName}:");
+
+                        var futureOptions = futureReservations.Select(r => $"Reservations: {r.Date} (ID: {r.ID})").ToList();
+                        var selectedFuture = SelectionPresent.Show(futureOptions, "FUTURE/CURRENT RESERVATIONS").text;
+
+                        if (futureOptions.Contains(selectedFuture))
                         {
                             Console.Clear();
-                            System.Console.WriteLine($"Here are your current and future reservations, {acc.FirstName}:");
-
-                            for (int j = 0; j < futureReservations.Count; j++)
-                            {
-                                if (j == futureIndex)
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Yellow;
-                                    Console.WriteLine($"> Reservation: {futureReservations[j].Date}"); // Highlight selected item
-                                    Console.ResetColor();
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"  Reservation: {futureReservations[j].Date}");
-                                }
-                            }
-
-                            var key = Console.ReadKey(intercept: true);
-                            switch (key.Key)
-                            {
-                                case ConsoleKey.UpArrow:
-                                    if (futureIndex > 0) futureIndex--; // Move up
-                                    break;
-
-                                case ConsoleKey.DownArrow:
-                                    if (futureIndex < futureReservations.Count - 1) futureIndex++; // Move down
-                                    break;
-
-                                case ConsoleKey.Enter:
-                                    // Process the selected reservation
-                                    Console.Clear();
-                                    Console.WriteLine($"You selected Reservation on: {futureReservations[futureIndex].Date}");
-                                    Console.WriteLine("Press any key to return to the reservation overview menu or press Escape to return to the main menu...");
-                                    var key2 = Console.ReadKey();
-
-                                    if (key2.Key == ConsoleKey.Escape)
-                                    {
-                                        inResMenu = false;
-                                        break; // Exit loop
-                                    }
-                                    else
-                                    {
-                                        break;
-                                    }
-
-                                case ConsoleKey.Escape: // Exit without selection
-                                    inResMenu = false;
-                                    break;
-                            }
+                            System.Console.WriteLine($"You selected Reservation on: {futureOptions}");
+                            Console.WriteLine("Press any key to return to the reservation overview menu or press Escape to return to the main menu...");
+                            Console.ReadKey();
                         }
                     break;
                     case "Cancel":
