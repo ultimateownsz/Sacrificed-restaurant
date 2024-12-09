36f350d247991cd9c64bf9709f78c7f4ec0b1556
ultimateownsz <saiutechnologies@outlook.com>
2024-12-09 20:31:13 +0100

    fix correct name

--- Restaurant App/Presentation/ThemeView.cs
+++ Restaurant App/Presentation/ThemeView.cs
@@ -15,7 +15,7 @@ static class ThemeView
             
             // Down here the selection menu for the months is done, rn u cant choose the current month
             string banner = $"Year: {year}\nSelect a month to edit the theme\n\n";            
-            List<string> options = Enumerable.Range(1, 12).Select(m => ThemeMenuManager.GetMonthThemeName(m, year)).ToList();
+            List<string> options = Enumerable.Range(1, 12).Select(m => ThemeMenuLogic.GetMonthThemeName(m, year)).ToList();
             int month;
             do
             {
@@ -30,23 +30,23 @@ static class ThemeView
 
             // This is to check if the chosen month has a theme, if not look at else
             // If it does have a theme then u enter a new mini menu
-            if(ThemeMenuManager.GetThemeByYearAndMonth(month, year) is not null)
+            if(ThemeMenuLogic.GetThemeByYearAndMonth(month, year) is not null)
             {
                 // In this menu u choose what happens with the chosen month (that does have a theme attached to it)
-                string banner2 = $"{ThemeMenuManager.GetMonthName(month)}\nChoose:\n\n";
+                string banner2 = $"{ThemeMenuLogic.GetMonthName(month)}\nChoose:\n\n";
                 List<string> options2 = new List<string>{"Edit the theme for this month", "Delete the theme for this month"};
                 int selection = SelectionPresent.Show(options2, banner2, false).index;
                 // This is for updating the theme the else is for deleting
                 if(selection == 0)
                 {
                     themeName = ThemeInputValidator.GetValidString();
-                    ThemeMenuManager.UpdateThemeSchedule(month, year, themeName);
+                    ThemeMenuLogic.UpdateThemeSchedule(month, year, themeName);
                     // Console.Clear();
                     Console.WriteLine($"The theme has been updated to {themeName}");
                 }
                 else 
                 {
-                    ThemeMenuManager.DeleteMonthTheme(month, year);
+                    ThemeMenuLogic.DeleteMonthTheme(month, year);
                     Console.Clear();
                     Console.WriteLine("This theme has been deleted");
                 }
@@ -54,7 +54,7 @@ static class ThemeView
             else
             {
                 themeName = ThemeInputValidator.GetValidString();
-                ThemeMenuManager.UpdateThemeSchedule(month, year, themeName);
+                ThemeMenuLogic.UpdateThemeSchedule(month, year, themeName);
                 Console.WriteLine($"The theme has been updated to {themeName}");
 
             }
