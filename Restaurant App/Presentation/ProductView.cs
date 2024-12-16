1c93bc8248a9a68e238ba4481173744b08cf5c44
Hani <hanimohamedhm911@gmail.com>
2024-12-16 05:00:12 +0100

    Editing Price Function
    -similar to EditProcutName, instead updates price

--- Restaurant App/Presentation/ProductView.cs
+++ Restaurant App/Presentation/ProductView.cs
@@ -17,6 +17,7 @@ static class ProductView
 
         while (true)
         {
+            Console.Clear();
             switch (SelectionPresent.Show(options, banner).text)
             {
                 case "Add product":
@@ -61,10 +62,8 @@ static class ProductView
             {
                 return;
             }
-            break;
+            DeleteOrEditChoice(chosenProduct);
         }
-        DeleteOrEditChoice(chosenProduct);
-        return;
     }
 
     public static void DeleteOrEditChoice(ProductModel chosenProduct)
@@ -107,7 +106,7 @@ static class ProductView
 
     public static void EditProductName(ProductModel oldProduct)
     {
-        string productNewName;
+        string newProductName;
         while(true)
         {        
             Console.Clear();
@@ -115,16 +114,16 @@ static class ProductView
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write("Enter new product name: ", Console.ForegroundColor);
             Console.ForegroundColor = ConsoleColor.White;
-            productNewName = Console.ReadLine();
+            newProductName = Console.ReadLine();
 
-            if (!string.IsNullOrWhiteSpace(productNewName) && !productNewName.Any(char.IsDigit))
+            if (!string.IsNullOrWhiteSpace(newProductName) && !newProductName.Any(char.IsDigit))
             {
                 break;
             }
             
             Console.Clear();            
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Enter new product name: {productNewName}", Console.ForegroundColor);
+            Console.WriteLine($"Enter new product name: {newProductName}", Console.ForegroundColor);
             
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("\nInvalid product name...");
@@ -133,20 +132,87 @@ static class ProductView
         ProductModel newProduct = new ProductModel
         {
             ID = oldProduct.ID,
-            Name = productNewName,
-            Price = Math.Round(decimal.Parse(oldProduct.Price.ToString()), 2),
+            Name = newProductName,
+            Price = oldProduct.Price,
             Course = oldProduct.Course,
             ThemeID = oldProduct.ThemeID
         };
 
-        ProductManager.UpdateProduct(oldProduct, newProduct);
-        
+        if(ProductManager.UpdateProduct(oldProduct, newProduct))
+        {
+            Console.WriteLine($"Name has been updated from {oldProduct.Name} to {newProductName}");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine($"Name failed to update");
+            Console.WriteLine("Press any key to continue..."); 
+            Console.ReadKey(); 
+        }
+
         return;
     }
 
     public static void EditProductPrice(ProductModel oldProduct)
     {
+        string newProductPriceString;
+        while(true)
+        {        
+            Console.Clear();
 
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Enter new product price: ", Console.ForegroundColor);
+            Console.ForegroundColor = ConsoleColor.White;
+            newProductPriceString = Console.ReadLine();
+
+            decimal temp;
+
+            if 
+            (
+                !string.IsNullOrWhiteSpace(newProductPriceString)
+                && decimal.TryParse(newProductPriceString, out temp)
+                && newProductPriceString.Contains('.')
+                && newProductPriceString.Trim().Split('.')[1].Length == 2
+                && !newProductPriceString.Contains(' ')
+            )
+            {
+                break;
+            }
+            
+            Console.Clear();            
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Enter new product price: {newProductPriceString}", Console.ForegroundColor);
+            
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nInvalid product price...");
+            Console.ReadKey();
+        }
+
+        decimal newProductPriceDecimal = Convert.ToDecimal(newProductPriceString);
+        ProductModel newProduct = new ProductModel
+        {
+            ID = oldProduct.ID,
+            Name = oldProduct.Name,
+            Price = newProductPriceDecimal,
+            Course = oldProduct.Course,
+            ThemeID = oldProduct.ThemeID
+        };
+
+        if(ProductManager.UpdateProduct(oldProduct, newProduct))
+        {
+            Console.WriteLine($"Price has been updated from {oldProduct.Price}€ to {newProductPriceDecimal}€");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine($"Price failed to update");
+            Console.WriteLine("Press any key to continue..."); 
+            Console.ReadKey(); 
+        }
+        
+        return;
     }
 
     public static void EditProductCourse(ProductModel oldProduct)
