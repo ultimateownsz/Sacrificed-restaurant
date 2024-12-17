249af780dc04ada6dc9a0cb83c7270528ef4c2b3
Hani <hanimohamedhm911@gmail.com>
2024-12-17 14:43:23 +0100

    again capitalizing the first letter this time in GetThemeIDByName

--- Restaurant App/Logic/ThemeMenuManager.cs
+++ Restaurant App/Logic/ThemeMenuManager.cs
@@ -130,7 +130,7 @@ static class ThemeMenuManager
     
     public static int? GetThemeIDByName(string name)
     {
-        var theme = Access.Themes.GetBy<string>("Name", name);
+        var theme = Access.Themes.GetBy<string>("Name", name.Substring(0, 1).ToUpper() + name.Substring(1));
         return theme?.ID;
     }
 
