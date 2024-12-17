0a47ab627be4d94894ac2c32ef057704dfd406b8
Hani <hanimohamedhm911@gmail.com>
2024-12-17 16:36:33 +0100

    Using a Menu for course selection
    -Instead of writing the name of the course, u just scroll through them instead

--- Restaurant App/Logic/CourseLogic.cs
+++ Restaurant App/Logic/CourseLogic.cs
@@ -1,34 +1,24 @@
 // logic for filtering products by category
 // this is used to filter products by category type in the ProductView.cs file
 
+using Project;
+
 public class CourseLogic
 {
-    public static string GetValidString()
+    public static string GetValidCourse()
     {
-        List<string> courses = new List<string>{"main", "dessert", "appetizer", "beverage"};
+        List<string> courses = new List<string>{"Main", "Dessert", "Appetizer", "Beverage"};
+
         while (true)
         {
-            Console.Clear();
+            string banner = "Choose course:\n\n";
+            var courseName = SelectionPresent.Show(courses, banner, false).text;
 
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("Enter course name: ", Console.ForegroundColor);
-            Console.ForegroundColor = ConsoleColor.White;
-            var courseName = Console.ReadLine().ToLower();
-
-            if (!string.IsNullOrWhiteSpace(courseName) && !courseName.Any(char.IsDigit) && courses.Contains(courseName))
+            if (courseName != "")
             {
-                return char.ToUpper(courseName[0]) + courseName.Substring(1);
+                return courseName;
             }
-
-            Console.Clear();            
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Enter vourse name: {courseName}", Console.ForegroundColor);
-            
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("\nInvalid course name...");
-            Console.WriteLine("Press any key to retry or ESCAPE to go back");
-            var key = Console.ReadKey(intercept: true);
-            if (key.Key == ConsoleKey.Escape || key.Key == ConsoleKey.B)
+            else
             {
                 return null;
             }
