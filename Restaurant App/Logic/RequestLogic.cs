eb9c18893d30ad47d1d0641e454b7bf5d5ac57ee
Davidek523 <davidov520@wp.pl>
2024-12-18 11:11:52 +0100

    Adding methods
    Added:
    - Two new methods in Request logic for generating menu options for page system and a method for formating the account (message)

--- Restaurant App/Logic/RequestLogic.cs
+++ Restaurant App/Logic/RequestLogic.cs
@@ -2,5 +2,17 @@ using Project;
 
 public class RequestLogic
 {
-    
+    public static string FormatAccount(ReservationModel reservation)
+    {
+        return $"Reservation on {reservation.Date:yyyy-MM-dd} at Table {reservation.PlaceID} (ID: {reservation.ID})";
+    }
+
+    public static List<string> GenerateMenuOptions(List<ReservationModel> accounts, int currentPage, int totalPages)
+    {
+        var options = accounts.Select(FormatAccount).ToList();
+        if (currentPage > 0) options.Add("Previous Page");
+        if (currentPage < totalPages - 1) options.Add("Next Page");
+        options.Add("Back");
+        return options;
+    }
 }
