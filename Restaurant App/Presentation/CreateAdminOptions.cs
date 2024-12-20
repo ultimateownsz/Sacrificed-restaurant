a15cda93d62ec8a001a11e3a5670008c4367c77c
Dyl4n01 <dylanspilt01@gmail.com>
2024-12-20 10:32:36 +0100

    Create admin menu
    Made a menu for creating an admin account. Called the create account method here

---
+++ Restaurant App/Presentation/CreateAdminOptions.cs
@@ -0,0 +1,31 @@
+namespace Project.Presentation
+{
+    public static class CreateAdminOptions
+    {
+        public static void ManageAdmins(UserModel acc)
+        {
+            List<string> adminOptions = new()
+            {
+                "Create a new admin account",
+                "Make an existing user an admin",
+                "Back"
+            };
+
+            string choice = SelectionPresent.Show(adminOptions, "Create (admin account)\n\n").text;
+
+            switch (choice)
+            {
+                case "Create a new admin account":
+                    RegisterUser.CreateAccount(true);  // Call the existing CreateAccount method with admin=true
+                    break;
+
+                case "Make an existing user an admin":
+                    // Add new function
+                    break;
+
+                case "Back":
+                    return;
+            }
+        }
+    }
+}
