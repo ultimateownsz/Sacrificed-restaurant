d98f68c7b8679798528d29d6f6c99ed2ffa98cfb
Dyl4n01 <dylanspilt01@gmail.com>
2025-01-05 12:18:10 +0100

    Added a warning
    Made a warning for the user when deleting account

--- Restaurant App/Presentation/DeleteAccountAsUser.cs
+++ Restaurant App/Presentation/DeleteAccountAsUser.cs
@@ -6,7 +6,11 @@ namespace Presentation
     {
         public static void DeleteAccount(UserModel user)
         {
-            
+            Console.Clear();
+            Console.WriteLine("WARNING: Deleting your account will:");
+            Console.WriteLine("- Delete all future reservations.");
+            Console.WriteLine("- Make your past reservations anonymous.");
+            Console.WriteLine("- Remove all your personal information.\n");
         }
     }
 }
