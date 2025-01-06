1d67d7a8f8c8e300579578d892af138834ca1a83
Davidek523 <davidov520@wp.pl>
2025-01-06 20:13:37 +0100

    Capitals
    Fixed:
    - capitals for consistency

--- Restaurant App/Logic/RequestLogic.cs
+++ Restaurant App/Logic/RequestLogic.cs
@@ -10,8 +10,8 @@ public class RequestLogic
     public static List<string> GenerateMenuOptions(List<RequestModel> accounts, int currentPage, int totalPages)
     {
         var options = accounts.Select(FormatAccount).ToList();
-        if (currentPage > 0) options.Add("Previous Page");
-        if (currentPage < totalPages - 1) options.Add("Next Page");
+        if (currentPage > 0) options.Add("Previous page");
+        if (currentPage < totalPages - 1) options.Add("Next page");
         options.Add("Back");
         return options;
     }
