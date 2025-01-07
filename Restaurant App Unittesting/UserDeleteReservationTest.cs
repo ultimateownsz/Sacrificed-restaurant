fba358c4fb8328bcc6442d1d10f1078b8acc5f38
Dyl4n01 <dylanspilt01@gmail.com>
2025-01-07 15:06:12 +0100

    Finished first test
    I will test it after i made the second test 👍

--- Restaurant App Unittesting/UserDeleteReservationTest.cs
+++ Restaurant App Unittesting/UserDeleteReservationTest.cs
@@ -14,7 +14,29 @@ namespace Restaurant_App_Unittesting
         [DataRow(2, "Yes", false)] // Reservation does not exist and is not deleted
         public void TestDeleteReservation_ConfirmYes(int reservationId, string userConfirmation, bool expectedResult)
         {
+            // Mocking the ReservationAccess to simulate reservation data
+            var mockReservations = new Dictionary<int, string>
+            {
+                { 1, "Reservation for Table 1" },
+                { 3, "Reservation for Table 3" }
+            };
 
+            // Simulating the DeleteReservation method logic
+            bool DeleteReservation(int id, string confirmation)
+            {
+                if (mockReservations.ContainsKey(id) && confirmation == "Yes")
+                {
+                    mockReservations.Remove(id); // Remove reservation if user confirms "Yes"
+                    return true;
+                }
+                return false; // Return false if the reservation doesn't exist or user cancels
+            }
+
+            // Act
+            bool result = DeleteReservation(reservationId, userConfirmation);
+
+            // Assert 
+            Assert.AreEqual(expectedResult, result);
         }
 
         // Unit Test for Criteria: User selects "No" on the delete confirmation
