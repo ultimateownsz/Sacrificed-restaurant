cb89b0fe342888baf50e8f417682ae2dd082fff4
Dyl4n01 <dylanspilt01@gmail.com>
2025-01-07 15:49:52 +0100

    Classes
    Added the three classes for the unit test

---
+++ Restaurant App Unittesting/ConvertUserToAdminTest.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Restaurant_App_Unittesting
+{
+    // Unit Test for Criteria 5: Database Update
+    [TestClass]
+    public class ConvertUserToAdminTest
+    {
+
+    }
+
+    // Mocking data access layer for testing
+    public class MockDataAccess
+    {
+
+    }
+
+    // User model class
+    public class UserModel
+    {
+        
+    }
+}
