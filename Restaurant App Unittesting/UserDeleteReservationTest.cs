bac67175f796c913f5b8d658d26b3db351ebb4ea
Dyl4n01 <dylanspilt01@gmail.com>
2025-01-07 14:57:50 +0100

    2 tests
    Made a start for the 2 tests I put in the excel sheet

--- Restaurant App Unittesting/UserDeleteReservationTest.cs
+++ Restaurant App Unittesting/UserDeleteReservationTest.cs
@@ -8,6 +8,22 @@ namespace Restaurant_App_Unittesting
     [TestClass]
     public class UserDeleteReservationTest
     {
-        
+        // Unit Test for criteria: User selects "Yes" on the delete confirmation
+        [TestMethod]
+        [DataRow(1, "Yes", true)] // Reservation exists and is deleted
+        [DataRow(2, "Yes", false)] // Reservation does not exist and is not deleted
+        public void TestDeleteReservation_ConfirmYes(int reservationId, string userConfirmation, bool expectedResult)
+        {
+
+        }
+
+        // Unit Test for Criteria: User selects "No" on the delete confirmation
+        [TestMethod]
+        [DataRow(1, "No", false)]  // Reservation exists but user cancels
+        [DataRow(2, "No", false)]  // Reservation does not exist and is not deleted
+        public void TestDeleteReservation_ConfirmNo(int reservationId, string userConfirmation, bool expectedResult)
+        {
+
+        }
     }
 }
