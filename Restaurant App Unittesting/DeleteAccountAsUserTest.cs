1a2f6500b8285dc29a7c1f4fee5e940e3b983313
Dyl4n01 <dylanspilt01@gmail.com>
2025-01-07 17:08:09 +0100

    3 unit tests
    Made the classes for the 3 unit tests

---
+++ Restaurant App Unittesting/DeleteAccountAsUserTest.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Restaurant_App_Unittesting
+{
+    // Unit Test for Criteria 1: Past Reservations are Saved
+    [TestClass]
+    public class PastReservations
+    {
+
+    }
+
+    // Unit Test for Criteria 4: Personal Information is Made Anonymous
+    [TestClass]
+    public class AnonymizeInfo
+    {
+
+    }
+
+    // Unit Test for Criteria 5: Ask User for Password Confirmation When Deleting Account
+    [TestClass]
+    public class PasswordConfirmation
+    {
+
+    }
+}
