2735a05ecddd4de0666437dca04da105ac655958
tr-arman <armanakcay@live.nl>
2025-01-12 19:41:24 +0100

    save 1

---
+++ App/Logic/TableSelectionLogic.cs
@@ -0,0 +1,123 @@
+namespace App.Logic.Table
+{
+    public static class TableSelectionLogic
+    {
+        public static async Task FlashHighlightAsyncLogic(
+            int tableNumber, int x, int y, ConsoleColor tableColor, CancellationToken token,
+            Action<int, int, string> setCursor, Func<int, int, string> restoreNumber)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                setCursor(x, y, "X ");
+                await Task.Delay(500);
+
+                if (token.IsCancellationRequested) break;
+
+                string originalNumber = restoreNumber(x, y);
+                setCursor(x, y, originalNumber);
+                await Task.Delay(500);
+            }
+        }
+
+        public static void ClearGridLogic(char[,] grid, Action<int, int, string> clearLine)
+        {
+            for (int y = 0; y < grid.GetLength(0); y++)
+            {
+                clearLine(0, y, new string(' ', grid.GetLength(1)));
+            }
+        }
+
+        public static (int x, int y) FindTableCoordinatesLogic(char[,] grid, int tableNumber, Func<int, int, string> getNumberAt)
+        {
+            for (int y = 0; y < grid.GetLength(0); y++)
+            {
+                for (int x = 0; x < grid.GetLength(1); x++)
+                {
+                    string number = getNumberAt(x, y);
+                    if (!string.IsNullOrEmpty(number) && int.Parse(number) == tableNumber)
+                    {
+                        return (x, y);
+                    }
+                }
+            }
+            throw new Exception($"Table {tableNumber} not found in the grid.");
+        }
+
+        public static (int x, int y) FindFirstAvailableCoordinatesLogic(
+            char[,] grid, int[] activeTables, int[] inactiveTables, int[] reservedTables, int guestCount,
+            Func<int, int, string> getNumberAt, Func<int, int, int[], bool> isTableValidForGuests)
+        {
+            for (int y = 0; y < grid.GetLength(0); y++)
+            {
+                for (int x = 0; x < grid.GetLength(1); x++)
+                {
+                    string number = getNumberAt(x, y);
+                    if (!string.IsNullOrEmpty(number))
+                    {
+                        int tableNumber = int.Parse(number);
+                        if (!reservedTables.Contains(tableNumber) && !inactiveTables.Contains(tableNumber)
+                            && isTableValidForGuests(tableNumber, guestCount, activeTables))
+                        {
+                            return (x, y);
+                        }
+                    }
+                }
+            }
+            return FindTableCoordinatesLogic(grid, 1, getNumberAt);
+        }
+
+        public static (bool isValid, string message) ValidateTableLogic(
+            int tableNumber, int guestCount, int[] activeTables, int[] reservedTables, int[] inactiveTables)
+        {
+            if (reservedTables.Contains(tableNumber))
+                return (false, $"Table {tableNumber} is reserved.");
+            if (inactiveTables.Contains(tableNumber))
+                return (false, $"Table {tableNumber} is inactive.");
+            if (!activeTables.Contains(tableNumber))
+                return (false, $"Table {tableNumber} is not active.");
+            if (guestCount > 6)
+                return (false, $"Table {tableNumber} cannot accommodate more than 6 guests.");
+
+            return (true, string.Empty);
+        }
+
+        public static void ClearCursorAreaLogic(int x, int y, string content, ConsoleColor color, Action<int, int, string, ConsoleColor> render)
+        {
+            render(x, y, content, color);
+        }
+
+        public static int SelectTableLogic(
+            char[,] grid, int[] activeTables, int[] inactiveTables, int[] reservedTables, int guestCount,
+            Func<int, int, string> getNumberAt, Func<int, int, int[], bool> isTableValidForGuests,
+            Func<int[], int[], int[], int, (int, int)> findFirstAvailableCoordinates,
+            Func<int, int[], int[], int[], bool, (bool, string)> validateTable,
+            Action<int, int, ConsoleColor, string> updateHighlight,
+            Func<ConsoleKey> readKey)
+        {
+            var (cursorX, cursorY) = findFirstAvailableCoordinates(activeTables, inactiveTables, reservedTables, guestCount);
+            updateHighlight(cursorX, cursorY, ConsoleColor.Yellow, "X");
+
+            while (true)
+            {
+                ConsoleKey key = readKey();
+                if (key == ConsoleKey.Enter)
+                {
+                    string selectedNumber = getNumberAt(cursorX, cursorY);
+                    if (string.IsNullOrEmpty(selectedNumber)) continue;
+
+                    int tableNumber = int.Parse(selectedNumber);
+                    var (isValid, message) = validateTable(
+                        tableNumber, guestCount, activeTables, reservedTables, inactiveTables, false);
+
+                    if (!isValid)
+                    {
+                        Console.WriteLine(message);
+                        continue;
+                    }
+
+                    return tableNumber;
+                }
+            }
+        }
+    }
+}
--- App/Presentation/Table/TableSelectionPresent.cs
+++ App/Presentation/Table/TableSelectionPresent.cs
@@ -1,123 +1,94 @@
-using App.Logic.Table;
-using App.DataAccess.Utils;
-using Restaurant;
-
 namespace App.Presentation.Table
 {
     public class TableSelectionPresent
     {
-        private CancellationTokenSource flashCancellationTokenSource = new CancellationTokenSource();
+        private readonly char[,] grid;
+        private readonly Dictionary<int, ConsoleColor> tableColors = new();
+        private CancellationTokenSource flashCancellationTokenSource = new();
         private int cursorX, cursorY;
 
-        public int SelectedTable { get; private set; }
-
-        public void ShowGrid(int[] activeTables, int[] inactiveTables, int[] reservedTables, int guestCount, bool isAdminMode = false)
+        public TableSelectionPresent(char[,] grid)
         {
-            var grid = GridPresent.GetGrid();
-            var tableColors = TableSelectionLogic.DetermineTableColors(grid, activeTables, inactiveTables, reservedTables, guestCount, isAdminMode);
-
-            Console.Clear();
-
-            for (int y = 0; y < grid.GetLength(0); y++)
-            {
-                for (int x = 0; x < grid.GetLength(1); x++)
-                {
-                    string number = TableSelectionLogic.GetNumberAt(grid, x, y);
-                    if (!string.IsNullOrEmpty(number))
-                    {
-                        int tableNumber = int.Parse(number);
-                        Console.SetCursorPosition(x - (number.Length - 1), y);
-                        Console.ForegroundColor = tableColors[tableNumber];
-                        Console.Write(number);
-                    }
-                    else
-                    {
-                        Console.ResetColor();
-                        Console.SetCursorPosition(x, y);
-                        Console.Write(grid[y, x]);
-                    }
-                }
-            }
-
-            (cursorX, cursorY) = TableSelectionLogic.FindFirstAvailableCoordinates(grid, activeTables, inactiveTables, reservedTables, guestCount);
-            HighlightNumber(grid, activeTables, reservedTables, inactiveTables, guestCount, isAdminMode);
+            this.grid = grid;
         }
 
-        private void HighlightNumber(char[,] grid, int[] activeTables, int[] reservedTables, int[] inactiveTables, int guestCount, bool isAdmin)
-        {
-            if (flashCancellationTokenSource != null && !flashCancellationTokenSource.IsCancellationRequested)
-            {
-                flashCancellationTokenSource.Cancel();
-                flashCancellationTokenSource.Dispose();
-            }
-
-            flashCancellationTokenSource = new CancellationTokenSource();
-
-            string currentNumber = TableSelectionLogic.GetNumberAt(grid, cursorX, cursorY);
-
-            if (!string.IsNullOrEmpty(currentNumber))
-            {
-                int currentTable = int.Parse(currentNumber);
-
-                var (isValid, _, message) = TableSelectionLogic.ValidateTable(currentTable, guestCount, activeTables, reservedTables, inactiveTables, isAdmin);
-
-                if (!isAdmin && !string.IsNullOrEmpty(message))
-                {
-                    DisplayMessage(message);
-                }
-                else
-                {
-                    ClearErrorMessage();
-                }
-
-                _ = FlashHighlightAsync(currentTable, cursorX, cursorY, ConsoleColor.Yellow);
-            }
-        }
-
-        private async Task FlashHighlightAsync(int tableNumber, int x, int y, ConsoleColor tableColor)
+        public async Task FlashHighlightAsync(int tableNumber, int x, int y, ConsoleColor tableColor)
         {
             var token = flashCancellationTokenSource.Token;
+            await App.Logic.Table.TableSelectionLogic.FlashHighlightAsyncLogic(
+                tableNumber, x, y, tableColor, token,
+                (cx, cy, str) =>
+                {
+                    Console.SetCursorPosition(cx, cy);
+                    Console.Write(str);
+                },
+                (cx, cy) => GetNumberAt(cx, cy));
+        }
 
-            while (!token.IsCancellationRequested)
+        public void ClearGrid()
+        {
+            App.Logic.Table.TableSelectionLogic.ClearGridLogic(grid, (x, y, str) =>
             {
                 Console.SetCursorPosition(x, y);
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write("X ");
-                await Task.Delay(500);
+                Console.Write(str);
+            });
+        }
 
-                if (token.IsCancellationRequested) break;
+        public (int x, int y) FindTableCoordinates(int tableNumber)
+        {
+            return App.Logic.Table.TableSelectionLogic.FindTableCoordinatesLogic(grid, tableNumber, GetNumberAt);
+        }
 
-                Console.SetCursorPosition(x, y);
-                Console.ForegroundColor = tableColor;
-                Console.Write(tableNumber.ToString().PadRight(2));
-                await Task.Delay(500);
-            }
+        public (int x, int y) FindFirstAvailableCoordinates(int[] activeTables, int[] inactiveTables, int[] reservedTables, int guestCount)
+        {
+            return App.Logic.Table.TableSelectionLogic.FindFirstAvailableCoordinatesLogic(
+                grid, activeTables, inactiveTables, reservedTables, guestCount, GetNumberAt, IsTableValidForGuests);
+        }
 
-            if (!token.IsCancellationRequested)
+        public void ClearCursorArea(int x, int y, string content, ConsoleColor color)
+        {
+            App.Logic.Table.TableSelectionLogic.ClearCursorAreaLogic(x, y, content, color, (cx, cy, str, clr) =>
             {
-                Console.SetCursorPosition(x, y);
-                Console.ForegroundColor = tableColor;
-                Console.Write(tableNumber.ToString().PadRight(2));
-            }
+                Console.SetCursorPosition(cx, cy);
+                Console.ForegroundColor = clr;
+                Console.Write(str);
+                Console.ResetColor();
+            });
+        }
 
+        public int SelectTable(int[] activeTables, int[] inactiveTables, int[] reservedTables, int guestCount, char[,] grid)
+        {
+            return App.Logic.Table.TableSelectionLogic.SelectTableLogic(
+                grid, activeTables, inactiveTables, reservedTables, guestCount,
+                GetNumberAt, IsTableValidForGuests,
+                FindFirstAvailableCoordinates,
+                ValidateTable,
+                UpdateTableHighlight,
+                () => Console.ReadKey(true).Key);
+        }
+
+        private string GetNumberAt(int x, int y)
+        {
+            return App.Logic.Table.TableSelectionLogic.GetNumberAtLogic(grid, x, y);
+        }
+
+        private bool IsTableValidForGuests(int tableNumber, int guestCount, int[] activeTables)
+        {
+            return activeTables.Contains(tableNumber) && guestCount <= 6;
+        }
+
+        private void UpdateTableHighlight(int x, int y, ConsoleColor color, string content)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = color;
+            Console.Write(content);
             Console.ResetColor();
         }
 
-        private void DisplayMessage(string message)
+        private (bool isValid, string message) ValidateTable(int tableNumber, int[] activeTables, int[] reservedTables, int[] inactiveTables, int guestCount)
         {
-            Console.SetCursorPosition(0, GridPresent.GetGrid().GetLength(0) + 1);
-            Console.Write(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, GridPresent.GetGrid().GetLength(0) + 1);
-
-            Console.ForegroundColor = message.Contains("can be reserved") ? ConsoleColor.Green : ConsoleColor.Red;
-            Console.WriteLine(message);
-            Console.ResetColor();
-        }
-
-        private void ClearErrorMessage()
-        {
-            Console.SetCursorPosition(0, GridPresent.GetGrid().GetLength(0) + 1);
-            Console.Write(new string(' ', Console.WindowWidth));
+            return App.Logic.Table.TableSelectionLogic.ValidateTableLogic(
+                tableNumber, guestCount, activeTables, reservedTables, inactiveTables);
         }
     }
 }
