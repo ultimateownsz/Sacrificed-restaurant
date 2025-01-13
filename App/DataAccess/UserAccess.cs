1f2cf0a3a72ba252ed91f24cba082404d278de38
Damarion Abendanon <da.abendanon@gmail.com>
2025-01-13 17:33:04 +0100

    finish last unit-test for foreign key deletion

--- App/DataAccess/UserAccess.cs
+++ App/DataAccess/UserAccess.cs
@@ -1,5 +1,6 @@
 ﻿using App.DataAccess.Utils;
 using App.DataModels.Allergy;
+using App.DataModels.Utils;
 
 namespace Restaurant;
 public class UserAccess: DataAccess<UserModel>
@@ -8,14 +9,16 @@ public class UserAccess: DataAccess<UserModel>
 
     public new bool Delete(int? id)
     {
-        IEnumerable<ReservationModel> reservations =
-            Access.Reservations.Read().Where(res => res.UserID == id);
 
-        foreach (var reservation in reservations)
-        {
-            if (!Access.Reservations.Delete(reservation.ID))
+        // first remove all reservations
+        foreach (var res in Access.Reservations.Read().Where(x => x.UserID == id))
+            if (!Access.Reservations.Delete(res.ID))
                 return false;
-        }
+
+        // ... and then remove all allerlinks
+        foreach (var lnk in Access.Allerlinks.Read().Where(x => x.EntityID == id && x.Personal == 1))
+            if (!Access.Allerlinks.Delete(lnk.ID))
+                return false;
 
         return base.Delete(id);
     }
