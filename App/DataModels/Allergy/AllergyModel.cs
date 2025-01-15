7da5f259afb2fab95ff72891e7799777c8e6f6aa
tr-arman <armanakcay@live.nl>
2025-01-15 09:36:27 +0100

    Removed unused constructor

--- App/DataModels/Allergy/AllergyModel.cs
+++ App/DataModels/Allergy/AllergyModel.cs
@@ -5,8 +5,6 @@ public class AllergyModel : IModel
 {
     public int? ID { get; set; }
     public string? Name { get; set; }
-
-    public AllergyModel() { }
     public AllergyModel(string? name, int? id = null)
     {
         ID = id;
