148048053966faa6f83e5fa4b6e7ddc4205cef24
tr-arman <armanakcay@live.nl>
2025-01-15 09:37:56 +0100

    ressurrected the removed models xd

--- App/DataModels/Allergy/AllergyModel.cs
+++ App/DataModels/Allergy/AllergyModel.cs
@@ -5,6 +5,8 @@ public class AllergyModel : IModel
 {
     public int? ID { get; set; }
     public string? Name { get; set; }
+
+    public AllergyModel() { }
     public AllergyModel(string? name, int? id = null)
     {
         ID = id;
--- App/DataModels/Product/PairModel.cs
+++ App/DataModels/Product/PairModel.cs
@@ -7,6 +7,7 @@ public class PairModel : IModel
     public int? FoodID { get; set; }
     public int? DrinkID { get; set; }
 
+    public PairModel() { }
     public PairModel(int? foodID, int? drinkID, int? id = null)
     {
         ID = id;
