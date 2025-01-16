6562d536fe4f8b6fcfd335366b18bc28479f3d90
Hani <hanimohamedhm911@gmail.com>
2025-01-16 22:59:47 +0100

    file for product editing testing

---
+++ App.Tests/ProductTesting.cs
@@ -0,0 +1,13 @@
+using App.DataAccess;
+using Restaurant;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App.Logic;
+
+namespace App.Tests
+{
+    [TestClass]
+    public class ProductEditTest
+    {
+
+    }
+}
