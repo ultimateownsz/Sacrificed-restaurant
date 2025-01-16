dc7d92d721e9cd693a43ec61621165e9897e6de2
Hani <hanimohamedhm911@gmail.com>
2025-01-16 23:21:44 +0100

    added the info

--- App.Tests/ProductTesting.cs
+++ App.Tests/ProductTesting.cs
@@ -2,6 +2,8 @@ using App.DataAccess;
 using Restaurant;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using App.Logic;
+using App.DataAccess.Product;
+using App.DataModels.Product;
 
 namespace App.Tests
 {
@@ -12,14 +14,28 @@ namespace App.Tests
         [TestMethod]
         public void Test_AddProduct()
         {
+            // Arrange
 
-        }
+            ProductModel newProduct = new ProductModel { ID = null, Name ="Meatballs", Price = 6.99m, Course = "Appetizer", ThemeID = 1 };
 
-        [TestMethod]
-        public void Test_EditProduct()
-        {
+            ProductAccess productAccess = new ProductAccess();
+
+            foreach (var product in productAccess.GetAll())
+            {
+                productAccess.Delete(product.ID);
+            }
             
+            productAccess.Write(new ProductModel { ID = 1, Name ="Pizza", Price = 12.99m, Course = "Main", ThemeID = 1 });
+            productAccess.Write(new ProductModel { ID = 2, Name ="Spaghetti", Price = 10.99m, Course = "Main", ThemeID = 1 });
+            productAccess.Write(new ProductModel { ID = 3, Name ="Cake", Price = 5.99m, Course = "Dessert", ThemeID = 1 });
+            productAccess.Write(new ProductModel { ID = 4, Name ="Beer", Price = 2.99m, Course = "Beverage", ThemeID = 1 });
+            productAccess.Write(new ProductModel { ID = 5, Name ="Wine", Price = 4.99m, Course = "Beverage", ThemeID = 1 });
+            productAccess.Write(new ProductModel { ID = 6, Name ="Salad", Price = 9.99m, Course = "Appetizer", ThemeID = 1 });
+
+
         }
+
+
 
     }
 }
